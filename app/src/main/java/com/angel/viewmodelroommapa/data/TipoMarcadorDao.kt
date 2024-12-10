5b16a7db49e35b2d14a3a58d86402775b2f1e315
ghuali <guaghuali@gmail.com>
2024-12-10 13:35:52 +0000

    completado TiposMarcadores

--- app/src/main/java/com/angel/viewmodelroommapa/data/TipoMarcadorDao.kt
+++ app/src/main/java/com/angel/viewmodelroommapa/data/TipoMarcadorDao.kt
@@ -1,4 +1,19 @@
 package com.angel.viewmodelroommapa.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface TipoMarcadorDao {
+
+    @Insert
+    suspend fun insertTipoMarcador(tipoMarcador: TipoMarcador)
+
+
+    @Query("SELECT * FROM TiposMarcadores")
+    fun getAllTipos(): Flow<List<TipoMarcador>>
+
 }
