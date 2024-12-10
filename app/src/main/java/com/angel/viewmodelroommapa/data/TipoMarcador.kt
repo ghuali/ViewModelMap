f7943c0b53934840e8628a2e8d7e37b8de127f5a
ghuali <guaghuali@gmail.com>
2024-12-10 13:34:06 +0000

    completado el TipoMarcador

--- app/src/main/java/com/angel/viewmodelroommapa/data/TipoMarcador.kt
+++ app/src/main/java/com/angel/viewmodelroommapa/data/TipoMarcador.kt
@@ -1,4 +1,10 @@
 package com.angel.viewmodelroommapa.data
 
-class TipoMarcador {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "TiposMarcadores")
+data class TipoMarcador(
+    @PrimaryKey(autoGenerate = true) val idTipoMarcador: Int = 0,
+    val tituloTipoMarcador: String
+)
