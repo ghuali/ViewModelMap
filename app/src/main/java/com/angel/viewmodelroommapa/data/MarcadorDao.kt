1a83eaade60d9de4ca256fc4cafad65c7746b20d
ghuali <guaghuali@gmail.com>
2024-12-10 13:52:10 +0000

    completado MarcadorDao con el nuevo MarcadorWithTipo

--- app/src/main/java/com/angel/viewmodelroommapa/data/MarcadorDao.kt
+++ app/src/main/java/com/angel/viewmodelroommapa/data/MarcadorDao.kt
@@ -5,10 +5,15 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface MarcadorDao {
 
     @Insert
-    suspend fun insertMarcador(marcador: Marcador)}
+    suspend fun insertMarcador(marcador: Marcador)
+
+@Query("SELECT * FROM MARCADORES")
+fun getAllMarcadoresAndTipos(): Flow<List<MarcadorWithTipo>>
+}
