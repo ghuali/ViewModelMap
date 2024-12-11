bf14b6f3d856506cbb709e8209deb23e8db09aa3
ghuali <guaghuali@gmail.com>
2024-12-11 08:45:43 +0000

    aplicado mapa en view

--- app/src/main/java/com/angel/viewmodelroommapa/view/View.kt
+++ app/src/main/java/com/angel/viewmodelroommapa/view/View.kt
@@ -1,2 +1,13 @@
 package com.angel.viewmodelroommapa.view
 
+val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
+    "Google-Sat",
+    0, 19, 256, ".png", arrayOf(
+        "https://mt0.google.com",
+        "https://mt1.google.com",
+        "https://mt2.google.com",
+        "https://mt3.google.com"
+    )
+) {
+
+}
