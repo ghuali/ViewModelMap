268125330c12445a8d4389f65234da9d4055f0dc
ghuali <guaghuali@gmail.com>
2024-12-11 09:12:39 +0000

    creamos funcion composable para el mainActivity

--- app/src/main/java/com/angel/viewmodelroommapa/view/View.kt
+++ app/src/main/java/com/angel/viewmodelroommapa/view/View.kt
@@ -1,5 +1,9 @@
 package com.angel.viewmodelroommapa.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.angel.viewmodelroommapa.ViewModel.MarcadorViewModel
+
 val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
     "Google-Sat",
     0, 19, 256, ".png", arrayOf(
@@ -9,8 +13,10 @@ val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
         "https://mt3.google.com"
     )
 ) { override fun getTileURLString(aTile: Long): String {
-    return baseUrl + "/vt/lyrs=s&x=" + MapTileIndex.getX(aTile) + "&y=" + MapTileIndex.getY(
+        return baseUrl + "/vt/lyrs=s&x=" + MapTileIndex.getX(aTile) + "&y=" + MapTileIndex.getY(
         aTile)
     }
-
 }
+
+@Composable
+fun MyMapView(modifier: Modifier = Modifier, viewModel: MarcadorViewModel) {}
