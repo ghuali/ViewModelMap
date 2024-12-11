6c94f2ba7048cd4575bcdfa7a24ec1a2a7df0b1d
ghuali <guaghuali@gmail.com>
2024-12-11 08:13:23 +0000

    completado el viewmodel

--- app/src/main/java/com/angel/viewmodelroommapa/ViewModel/ViewModel.kt
+++ app/src/main/java/com/angel/viewmodelroommapa/ViewModel/ViewModel.kt
@@ -1,4 +1,13 @@
 package com.angel.viewmodelroommapa.ViewModel
 
-class ViewModel {
+import androidx.lifecycle.ViewModel
+import com.angel.viewmodelroommapa.data.MarcadorDao
+import com.angel.viewmodelroommapa.data.MarcadorWithTipo
+
+import kotlinx.coroutines.flow.Flow
+
+class MarcadorViewModel(private val marcadorDao: MarcadorDao) : ViewModel() {
+
+    val marcadoresWithTipo: Flow<List<MarcadorWithTipo>> =
+        marcadorDao.getAllMarcadoresAndTipos()
 }
